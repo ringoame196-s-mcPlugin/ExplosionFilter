5615c543cf47bc0e337143c6df0730abe2d8a58e
RIngoame196 <132573268+Ringoame196@users.noreply.github.com>
2026-06-17 21:04:07 +0900

    feat:爆発関係

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/Events.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/Events.kt
@@ -1,5 +1,31 @@
 package com.github.ringoame196_s_mcPlugin.events
 
+import com.github.ringoame196_s_mcPlugin.ExplosionFilter
+import com.github.ringoame196_s_mcPlugin.FilterGUIManager
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
-class Events : Listener
+    @EventHandler
+    fun onFilterGUIClick(e: InventoryClickEvent) {
+        val gui = e.view
+        val inventory = e.inventory
+        val player = e.whoClicked as? Player ?: return
+        val item = e.currentItem ?: return
+        if (!FilterGUIManager.isFilterGUI(gui)) return
+
+        // nextアイテムで次のページへ移動
+        if (FilterGUIManager.isNextItem(item)) {
+            e.isCancelled = true
+            FilterGUIManager.nextApplicationFilterBlockList(inventory, player)
+        }
+    }
+
+    @EventHandler
+    fun onFilterGUIClose(e: InventoryCloseEvent) {
+        val gui = e.view
+        val inventory = e.inventory
+        if (!FilterGUIManager.isFilterGUI(gui)) return
+        FilterGUIManager.saveFilterBlockList(inventory) // GUIの内容をフィルターブロック設定として保存
+    }
+}
