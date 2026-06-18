f4720a33308b47ab3309bfca172e50d375dd9b1c
RIngoame196 <132573268+Ringoame196@users.noreply.github.com>
2026-06-18 11:27:52 +0900

    refactor : イベントクラスを種類ごとに分類

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/ExplosionEvents.kt
@@ -0,0 +1,13 @@
+package com.github.ringoame196_s_mcPlugin.events
+
+import com.github.ringoame196_s_mcPlugin.ExplosionFilter
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityExplodeEvent
+
+class ExplosionEvents : Listener {
+    @EventHandler
+    fun onEntityExplode(e: EntityExplodeEvent) {
+        ExplosionFilter.removeFilterBlock(e.blockList())
+    }
+}
